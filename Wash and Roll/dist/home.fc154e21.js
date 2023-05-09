cbf60b7ba3e83f20dc120b18079ca22edbbbbdb0
udie1011 <92238111+udie1011@users.noreply.github.com>
2023-05-08 23:50:39 -0400

    Add files via upload

---
+++ Wash and Roll/dist/home.fc154e21.js
@@ -0,0 +1,2 @@
+const liAbout=document.getElementById("liAbout"),liRefund=document.getElementById("liRefund"),sectionAbout=document.getElementById("sectionAbout"),sectionRefund=document.getElementById("sectionRefund");liAbout.addEventListener("click",(function(e){scrollTo(0,sectionAbout.offsetTop)})),liRefund.addEventListener("click",(function(e){scrollTo(0,sectionRefund.offsetTop)}));const home=document.getElementById("home");home.addEventListener("click",(function(e){scrollTo(0,0)}));
+//# sourceMappingURL=home.fc154e21.js.map
